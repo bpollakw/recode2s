7aa6e99e4fa2ebc9cb6b1578d74d93312d3ee888
Bernardo Pollak <bpollakw@gmail.com>
2016-08-22 19:21:00 +0100

    home

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, escape, request, render_template, make_response, flash, abort
+
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation, CompoundLocation
+from Bio.Alphabet import IUPAC
+
+
 
 app = Flask(__name__)
 
+def page_not_found(e):
+    return render_template('404.html', title='404'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+	return render_template('500.html', title='500'), 500
+
 @app.route("/")
 def index():
-  return "Hello World"
+	return render_template("home.html")
+
+
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
