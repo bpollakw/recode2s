5ab5a5ab141d08ed956ad351c409d57ed354a343
Bernardo Pollak <bpollakw@gmail.com>
2016-08-25 14:03:34 +0100

    static content

--- app.py
+++ app.py
@@ -143,6 +143,6 @@ def export():
 
 if __name__ == "__main__":
  	port = int(os.environ.get('PORT', 5000))	
-	serve(app, host="0.0.0.0", port=port)
+	serve(app, host="0.0.0.0", port=port, url_prefix='/static')
 	#serve(wsgiapp, listen='*:8080')  
 	#app.run(debug=True, host="0.0.0.0", port=5000)
